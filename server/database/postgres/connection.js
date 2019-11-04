9b7df2a0a6cbbec612a5e542c4b4e04046a8abbf
Jeffrey Lee <jglee1@msn.com>
2019-11-04 02:04:39 -0800

    Feature: setting up pg connection

--- server/database/postgres/connection.js
+++ server/database/postgres/connection.js
@@ -1,5 +1,5 @@
-//requirements
-require("dotenv").config();
+// requirements
+require('dotenv').config();
 const Pool = require('pg');
 
 const pool = new Pool({
@@ -7,7 +7,20 @@ const pool = new Pool({
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DATABASE,
   password: process.env.POSTGRES_PASSWORD,
-  port: process.env.POSTGRES_PORT
+  port: process.env.POSTGRES_PORT,
+});
+
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('error acquiring client', err.stack);
+  }
+  client.query('SELECT NOW()', (err, result) => {
+    release();
+    if (err) {
+      return console.error('Error executing query', err.stack);
+    }
+    console.log(result.rows);
+  });
 });
 
 module.exports = pool;
