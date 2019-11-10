0b49fd90f17a4afc157930ace9829b15645ffd53
Jeffrey Lee <jglee1@msn.com>
2019-11-09 17:14:55 -0800

    Feature: Data gen refactor complete.

--- server/database/postgres/schema.sql
+++ server/database/postgres/schema.sql
@@ -6,10 +6,7 @@ CREATE DATABASE movies;
 CREATE TABLE moviePreviews (
   id INT PRIMARY KEY NOT NULL,
   title VARCHAR(30) NOT NULL,
-  criticConsensus TEXT NOT NULL,
-  videoUrl VARCHAR(60) NOT NULL,
-  imgUrl VARCHAR(60) NOT NULL,
-  videoScene VARCHAR(60) NOT NULL
+  criticConsensus TEXT NOT NULL
 );
 
 CREATE TABLE audienceScore (
@@ -30,10 +27,19 @@ CREATE TABLE potatoeMeter (
   spoiled INT
 );
 
+CREATE TABLE media (
+  id INT PRIMARY KEY,
+  videoUrl TEXT,
+  imgUrl TEXT,
+  videoScene TEXT
+);
+
 COPY moviePreviews FROM '/Users/jeffreylee/hackreactor/Immersive/movie-preview/server/database/seed/moviePreviews.csv' CSV HEADER;
 
 COPY audienceScore FROM '/Users/jeffreylee/hackreactor/Immersive/movie-preview/server/database/seed/audienceScores.csv' CSV HEADER;
 
 COPY potatoeMeter FROM '/Users/jeffreylee/hackreactor/Immersive/movie-preview/server/database/seed/potatoMeter.csv' CSV HEADER;
+
+COPY media FROM '/Users/jeffreylee/hackreactor/Immersive/movie-preview/server/database/seed/media.csv' CSV HEADER;
 
 -- use psql postgres < schema.sql
